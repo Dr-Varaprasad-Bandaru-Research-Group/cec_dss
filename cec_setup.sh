45cf8c3baf14204a33792188e9962b8653381a81
UjjwalAyyangar <kulkarnip@gsapp1.gshpc.umd.edu>
2020-05-02 03:53:04 -0400

    added cec setup file

---
+++ cec_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Author - UjjwalAyyangar"
+echo "Adding modules for CEC project "
+module load R/3.5.3
+module load RStudio
+module load gdal/2.4.1
+module load proj4/4.8.0
+module load python/3.7/anaconda
+echo "Added all the modules successfully"
+cd / # path to cec folder
+
+
