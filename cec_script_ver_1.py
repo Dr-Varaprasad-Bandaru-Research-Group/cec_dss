c1f6d6ec31ae954dde0c65c4c2b916f5dfcfa83f
UjjwalAyyangar <kulkarnip@gsapp1.gshpc.umd.edu>
2020-05-02 13:48:46 -0400

    [WIP] Added stack

--- cec_script_ver_1.py
+++ cec_script_ver_1.py
@@ -13,31 +13,47 @@ def fetch(path):
 
 def load_preproc_raster(path):
 	dir = os.path.join("preproc_rasters",path)
-	return rasterio.open(dir)
+	return rasterio.open(dir).read()
 
+def stack_rasters(ras_list, dst):
 
+	print("getting meta data from {}".format(ras_list[0]))
+
+	with rasterio.open(ras_list[0]) as src0:
+		meta = src0.meta
+
+	meta.update(count=len(ras_list))
+
+	print('stacking and writing to {}'.format(dst))
+	
+	with rasterio.open(dst,'w',**meta) as dst:
+		for id, layer in enumerate(ras_list, start=1):
+			with rasterio.open(layer) as src1:
+				dst.write_band(id,src1.read(1))
 
 
 data_dir = "/gpfs/data1/cmongp/ujjwal/cec/Forest_Data/"
 
 total_path = "Total*.tif"
-output_path = "output_*.tif"
-sit_path = "ca_site_classes/ca_siteclcd_1_30m.tif"
-
 
 fl_files = fetch(total_path)
-output_files = fetch(output_path)
-sit = fetch(sit_path)
 
-print("total files are",fl_files)
-
-
+print('reading forest files')
 fl_1 = rasterio.open(fl_files[0])
 fl_elp = load_preproc_raster("ele_raster.tif")
 fl_slp = load_preproc_raster("slp_raster.tif")
 fl_asp = load_preproc_raster("asp_raster.tif")
 fl_sit = load_preproc_raster("sit_raster.tif")
 
+
+sit_path = os.path.join("preproc_rasters","sit_raster.tif")
+ras_list = [fl_files[0],sit_path]
+
+# stack_rasters(ras_list,'fl_stack.tif')
+print("reading stack")
+fl_stack = load_preproc_raster("fl_stack.tif")
+print("cdl prj")
+cdl_prj_mask = load_preproc_raster("cdl_pj.tif")
 
 
 
